bbd9fb178b02be2e1894b177eb15aa2be854be5b
Imtiyaz Pukhta <pukhtaib@gmail.com>
2021-08-25 19:43:06 +0530

    fixed scrolling color change nav button

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const menuIcon = document.getElementById("bars");
-menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("fa-bars");
-  menuIcon.classList.toggle("fa-times");
-  document.getElementById("navbar").classList.toggle("open");
+const menuIcon = document.getElementById('bars');
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('fa-bars');
+  menuIcon.classList.toggle('fa-times');
+  document.getElementById('navbar').classList.toggle('open');
 });
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName('mySlides');
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = 'block';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-var target = document.querySelector(".tabs");
-var targetTwo = document.querySelector(".header");
-document.addEventListener("scroll", () => {
-  if (window.scrollY >= target.getBoundingClientRect().top) {
-    document.querySelector(".menu-icon").style.color = "black";
-  }
-  if (window.scrollY <= targetTwo.getBoundingClientRect().top) {
-    document.querySelector(".menu-icon").style.color = "white";
-  }
+var target = document.querySelector('.tabs');
+var targetTwo = document.querySelector('.header');
+document.addEventListener('scroll', () => {
+  if (target.getBoundingClientRect().top < 40)
+    document.querySelector('.menu-icon').style.color = 'black';
+  else document.querySelector('.menu-icon').style.color = 'white';
 });
