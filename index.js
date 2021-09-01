134935479e2852b4791f3ef6d88b2c2a2676b655
Imtiyaz Pukhta <pukhtaib@gmail.com>
2021-09-01 13:49:57 +0530

    added images to slideshow

--- index.js
+++ index.js
@@ -6,24 +6,35 @@ menuIcon.addEventListener('click', () => {
 });
 var slideIndex = 0;
 showSlides();
+var slides,dots;
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName('mySlides');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 8 seconds
 }
-var target = document.querySelector('.tabs');
-var targetTwo = document.querySelector('.header');
-document.addEventListener('scroll', () => {
-  if (target.getBoundingClientRect().top < 40)
-    document.querySelector('.menu-icon').style.color = 'black';
-  else document.querySelector('.menu-icon').style.color = 'white';
-});
+
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
